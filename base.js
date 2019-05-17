1baf6c067d72f6513cd32ca8dd8fc8e1f378d191
zhangchao <529743595@qq.com>
2019-05-17 14:49:56 +0800

    拆分base.js 增加bom，dom，date

--- base.js
+++ base.js
@@ -210,48 +210,25 @@ var methods = {
     })
     return p
   },
-  gettoday() {
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var strDate = date.getDate();
-    if (month >= 1 && month <= 9) {
-      month = "0" + month;
+
+  // 改变伪元素的样式 // https://segmentfault.com/q/1010000002452755
+  ruleSelector(selector) {
+    function uni(selector) {
+      return selector.replace(/::/g, ':')
     }
-    if (strDate >= 0 && strDate <= 9) {
-      strDate = "0" + strDate;
-    }
-    var currentdate = '' + date.getFullYear() + month + strDate;
-    return currentdate;
-  },
-  gettodayfull() {
-    var date = new Date();
-    var seperator1 = "-";
-    var seperator2 = ":";
-    var month = date.getMonth() + 1;
-    var strDate = date.getDate();
-    var strhour = date.getHours();
-    var strmin = date.getMinutes();
-    var strsec = date.getSeconds();
-    if (month >= 1 && month <= 9) {
-      month = "0" + month;
-    }
-    if (strDate >= 0 && strDate <= 9) {
-      strDate = "0" + strDate;
-    }
-    if (strhour >= 0 && strhour <= 9) {
-      strhour = "0" + strhour;
-    }
-    if (strmin >= 0 && strmin <= 9) {
-      strmin = "0" + strmin;
-    }
-    if (strsec >= 0 && strsec <= 9) {
-      strsec = "0" + strsec;
-    }
-    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
-      "&" + strhour + seperator1 + strmin +
-      seperator1 + strsec + "&";
-    return currentdate;
-  },
+    // es6
+    return Array.from(document.styleSheets).reduce((a,b) => {
+      return a.concat(Array.from(b.cssRules))
+    }, []).filter(x => {
+      return uni(x.selectorText) === uni(selector);
+    })
+    // es5
+    // return Array.prototype.filter.call(Array.prototype.concat.apply([], Array.prototype.map.call(document.styleSheets, function (x) {
+    //   return Array.prototype.slice.call(x.cssRules);
+    // })), function (x) {
+    //   return uni(x.selectorText) === uni(selector);
+    // });
+  }
 }
 
 //类似jq append()方法。//高版本可用原生insertAdjacentHTML方法代替
